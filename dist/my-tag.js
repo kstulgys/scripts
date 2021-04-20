783f6f0cf490fa50f679fe43c31d490de31d3ee8
kstulgys <karolis.stulgys@gmail.com>
2021-04-20 19:23:21 +0000

    wip

--- dist/my-tag.js
+++ dist/my-tag.js
@@ -3,5 +3,6 @@
         console.log("hello world from git repo")
         const header = document.getElementById("shopify-section-header")
         header.style.backgroundColor = 'green'
+        console.log({ header })
     })
 })()
