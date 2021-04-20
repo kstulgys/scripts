dc75465dcde703a555b9b6753740bc0aa21d2290
kstulgys <karolis.stulgys@gmail.com>
2021-04-20 19:44:48 +0000

    www

--- dist/my-tag.js
+++ dist/my-tag.js
@@ -1,8 +1,8 @@
 // ((window) => {
-window.onload(() => {
-    console.log("hello world from git repo")
-    const header = document.getElementById("shopify-section-header")
-    header.style.backgroundColor = 'green'
-    console.log({ header })
-})
+// window.onload(() => {
+console.log("hello world from git repo")
+const header = document.getElementById("shopify-section-header")
+header.style.backgroundColor = 'green'
+console.log({ header })
+// })
 // })(window)
