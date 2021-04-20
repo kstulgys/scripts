fdc93d69fd3e5329bd851379a9ecbf707c138f1b
kstulgys <karolis.stulgys@gmail.com>
2021-04-20 19:07:50 +0000

    wip

--- dist/my-tag.js
+++ dist/my-tag.js
@@ -1,1 +1,5 @@
-console.log("hello world from git repo")
+(() => {
+    console.log("hello world from git repo")
+    const header = document.getElementById("shopify-section-header")
+    header.style.background = 'green'
+})()
